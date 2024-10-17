c39b79cf6d13f9096720a8e4b3bca58f3811372c
itsayno33 <itsayno33@gmail.com>
2024-10-18 06:28:43 +0900

    New Project of the "Some Of Customized Linux."

---
+++ __InitScripts/initScripts.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 環境変数の初期設定
+
+# 初期値peeというユーザを作成、初期値powというPWを設定
+export ${__SOCL_USER:="pee"}
+export ${__SOCL_PASS:="pow"}
+useradd -mN /bin/bash -s ${__SOCL_USER}
+echo "${__SOCL_USER}:${__SOCL_PASS}" | chpasswd
+
+# ユーザーをグループに追加(初期値は同名)
+export ${__SOCL_GROUP:=${__SOCL_USER}}
+groupadd -f ${__SOCL_GROUP}
+usermod  -g ${__SOCL_GROUP} ${__SOCL_USER}
+
+# sudoでroot権限のコマンドを実行できるように指定
+gpasswd -a ${__SOCL_USER} sudo
+
+# ビルド時に必要なスクリプトを置くフォルダの作成
+mkdir  -p /BuildScripts
+chgmod -R a=+x /BuildScripts
+chgown -R "${__SOCL_USER}:${__SOCL_GROUP}" /BuildScripts
+
+# その他いろいろ用のフォルダの作成
+mkdir  -pm 777 /BuildData
+chgown -R "${__SOCL_USER}:${__SOCL_GROUP}" /BuildData
