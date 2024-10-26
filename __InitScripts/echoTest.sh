441c86772079df4e57eb66f107f14209946bb757
itsayno33 <itsayno33@gmail.com>
2024-10-26 11:04:27 +0900

    BUg fixed

--- __InitScripts/echoTest.sh
+++ __InitScripts/echoTest.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash 
 
-declare -a TARG=( )
-for arg; do
-    TARG=( "${TARG[@]}" $arg )
-done
+#declare -a TARG=( )
+#for arg; do
+#    TARG=( "${TARG[@]}" $arg )
+#done
 IFS='|'
 echo "called: ${TARG[*]}" 1>&2
 
